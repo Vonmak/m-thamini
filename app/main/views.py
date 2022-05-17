39a5e3191e37b899907af049fc67a619d8600a90
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-17 14:10:16 +0300

    added the functions for pull table data in the views.py file.

---
+++ app/main/views.py
@@ -0,0 +1,27 @@
+from flask import render_template, redirect, url_for
+from . import main
+from ..models import User,Asset,Subscriber
+from .forms import AssetForm
+from flask_login import login_required, current_user
+
+
+@main.route('/assets/')
+def assets():
+    currentAssets = Asset.query.filter_by(category_id='Current Asset')
+    financialAssets = Asset.query.filter_by(category_id='Financial Asset')
+    fixedAssets = Asset.query.filter_by(category_id='Fixed Asset')
+    
+    return render_template('assets.html', currentAssets=currentAssets, financialAssets=financialAssets, fixedAssets=fixedAssets)
+
+@main.route('/assets/new')
+def assets_new():
+    form = AssetForm()
+    if form.validate_on_submit():
+        description = form.description.data
+        title = form.title.data
+        category_id = form.category_id.data
+        worth = form.worth.data
+        new_asset = Asset(user_id=current_user.id, title = title,description=description,category_id=category_id, worth=worth)
+        new_asset.save_asset()
+        return redirect(url_for('main.assets'))
+    return render_template('new_asset.html',form=form)
