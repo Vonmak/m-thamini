9e57d3ddcaeef12988443462344870e5efc6fb56
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-18 10:27:20 +0300

    added a form class for assets addition to our page.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField,SelectField, IntegerField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, IntegerField
 from wtforms.validators import Required
 
-    
 
 class AssetForm(FlaskForm):
     title = StringField('Title', validators=[Required()])
-    description =TextAreaField('How best would you describe your Asset?', validators=[Required()])
-    worth = IntegerField('worth', validators=[Required()])
-    category_id = SelectField('Label', choices=[('currentAssets','Current Assets'),('financialAssets', 'Financial Assets'),('fixedAssets','Fixed Assets')])
+    category_id = SelectField('Asset', choices=[('currentAssets', 'Current Assets'), (
+        'financialAssets', 'Financial Assets'), ('fixedAssets', 'Fixed Assets')])
+
+    description = TextAreaField(
+        'How best would you describe your Asset?', validators=[Required()])
+    location = StringField('Asset Location', validators=[Required()])
+    worth = IntegerField('Worth', validators=[Required()])
+
     submit = SubmitField('Submit')
