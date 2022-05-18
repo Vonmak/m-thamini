c2a686082a3855db23aa2e06fbac78807a0f5ac1
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-18 10:22:15 +0300

    defined bootstrap and initialized its instance.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-# from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 # from flask_uploads import UploadSet, configure_uploads, IMAGES
 # from flask_mail import Mail
@@ -11,34 +11,30 @@ login_manager = LoginManager()
 # login_manager.session_protection = 'strong'
 # login_manager.login_view = 'auth.login'
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 # photos = UploadSet('photos', IMAGES)
 # mail=Mail()
 # simple = SimpleMDE()
 
+
 def create_app(config_name):
     app = Flask(__name__)
-    
+
     app.config.from_object(config_options[config_name])
-    
-    # bootstrap.init_app(app)
+
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
     # mail.init_app(app)
     # simple.init_app(app)
-    
-    
+
     # configure_uploads(app, photos)
-    
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    
     # from .auth import auth as auth_blueprint
     # app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-    
-    
-    
+
     return app
-    
