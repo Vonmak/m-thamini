6b53661f84ce818a267cdb37abf23b5db927b21f
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-18 14:35:47 +0300

    updated the form labels

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,13 +4,13 @@ from wtforms.validators import Required
 
 
 class AssetForm(FlaskForm):
-    title = StringField('Title', validators=[Required()])
-    category_id = SelectField('Asset', choices=[('currentAssets', 'Current Assets'), (
+    assetname = StringField('Asset Name', validators=[Required()])
+    category_id = SelectField('Type of Assset', choices=[('currentAssets', 'Current Assets'), (
         'financialAssets', 'Financial Assets'), ('fixedAssets', 'Fixed Assets')])
 
     description = TextAreaField(
         'How best would you describe your Asset?', validators=[Required()])
-    location = StringField('Asset Location', validators=[Required()])
-    worth = IntegerField('Worth', validators=[Required()])
+    # location = StringField('Asset Location', validators=[Required()])
+    worth = IntegerField('Asset Value', validators=[Required()])
 
     submit = SubmitField('Submit')
