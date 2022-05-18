aea7359f3765171f27247b981d2bab1b97499fda
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-18 14:36:47 +0300

    still working on the assets additions function.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for,flash
 from . import main
-from ..models import Asset
+from ..models import Asset, User
 from .forms import AssetForm
 from flask_login import login_required, current_user
+from app import db
 
 
 @main.route('/')
@@ -23,17 +24,19 @@ def assets():
     return render_template('assets.html', assets=assets, title=title, currentAssets=currentAssets, financialAssets=financialAssets, fixedAssets=fixedAssets)
 
 
-@main.route('/assets/new')
-def assets_new():
+@main.route('/assets/<int:user_id>/new', methods=['GET', 'POST'])
+def assets_new(user_id):
     form = AssetForm()
     if form.validate_on_submit():
         description = form.description.data
-        title = form.title.data
+        assetname = form.assetname.data
         category_id = form.category_id.data
         worth = form.worth.data
-        location = form.location.data
-        new_asset = Asset(user_id=current_user.id, title=title,
-                          description=description, category_id=category_id, worth=worth, location=location)
-        new_asset.save_asset()
+        # location = form.location.data
+        # user_id= current_user
+        new_asset = Asset(user_id = user_id, assetname=assetname,
+                          description=description, category_id=category_id, worth=worth)
+        new_asset.save_assets()
+        flash('Your asset has been added successfully', 'success')
         return redirect(url_for('main.assets'))
     return render_template('new_asset.html', form=form)
