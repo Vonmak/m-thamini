c805582aaba61700ac7e29b575c3842008171cd8
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-19 17:44:45 +0300

    added the logout function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,38 +1,42 @@
 
-from flask import render_template,url_for,flash,request,redirect
+from flask import render_template, url_for, flash, request, redirect
 from . import auth
-from .forms import RegistrationForm,LoginForm
-from flask_login import login_user,logout_user,login_required
+from .forms import RegistrationForm, LoginForm
+from flask_login import login_user, logout_user, login_required
 from .. import db
 from ..models import User
 
 
-
-
-@auth.route('/register',methods=['GET','POST'])
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-      form = RegistrationForm()
-      if form.validate_on_submit():
-            user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('auth.login'))
-            title = "New Account"
-      
-      return render_template('register.html',registration_form = form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+
+    return render_template('register.html', registration_form=form)
 
 
-    
-@auth.route('/login',methods=['GET','POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-      login_form = LoginForm()
-      if login_form.validate_on_submit():
-            user = User.query.filter_by(email = login_form.email.data).first()
-            if user is not None and user.verify_password(login_form.password.data):
-                login_user(user,login_form.remember.data)
-                return redirect(request.args.get('next') or url_for('main.index'))
-            flash('Invalid username or Password') 
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
 
-      title = "login"
-      return render_template('login.html',title=title,login_form = login_form)
+    title = "login"
+    return render_template('login.html', title=title, login_form=login_form)
 
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
