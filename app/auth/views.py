15cf8e5bfb215b27d0edb5f4ddb87b65c4961bc3
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-19 09:15:17 +0300

    Updated registration, add assests and dashboard design and styles

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,3 +12,15 @@ def register():
 def login():
       
       return render_template('login.html')
+
+
+@auth.route('dashboard/tables')
+def tables():
+      
+      return render_template('dashboard/tables.html')
+
+
+@auth.route('dashboard/add_assets')
+def add_assets():
+      
+      return render_template('dashboard/add_assets.html')
