acd9c1f08e9fadd32c1cfe3d2ccf5376f54706de
Aprilwairimu <wairimuapril@gmail.com>
2022-07-14 12:50:16 +0300

    created forms and login/register function

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,12 +7,12 @@ from .. import db
 from ..models import User
 
 
-@auth.route('/register', methods=['GET', 'POST'])
+@auth.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email=form.email.data,
-                    username=form.username.data, password=form.password.data)
+                    username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
@@ -21,13 +21,13 @@ def register():
     return render_template('register.html', registration_form=form)
 
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login', methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
-            login_user(user, login_form.remember.data)
+            login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or Password')
 
